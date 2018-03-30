3ed11ad07f266c33b76abc2c3cd1e34d8c9e5e2b
paladin4ua <vkubiv@emp.org.ua>
2018-03-30 15:55:41 +0300

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
+declare var cordova : any;
+
+class Notification {
+  id: Number;
+  title: string;
+  text: string;
+  data: any;
+  at: Date;
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,8 +22,48 @@ export class HomePage {
 
   }
 
-  scheduleNotificatins() {
+  onScheduleClick() {
+
+    let time = new Date();
+    let notifications : Notification[] = [];
+
+    for (let i = 0; i < 10; ++i) {
+
+      time.setSeconds(time.getSeconds() + 10);
+
+      notifications.push({
+        id: i,
+        title: "Title " + i,
+        text: "text " + i,
+        data: { dummy : "some data"},
+        at: time
+      })
+    }
+
+    this.scheduleNotifications(notifications);
 
   }
+
+  scheduleNotifications(notifications : Notification[]) {
+
+    cordova.plugins.notification.local.clearAll((res) => {
+
+      let notificationsDesc = notifications.map((notification) => {
+        return {
+          id: notification.id,
+          title: notification.title,
+          text: notification.text,
+          data: notification.data,
+          trigger: { at: notification.at }
+        }
+      });
+
+      cordova.plugins.notification.local.schedule(notificationsDesc);
+    });
+
+  }
+
+
+
 
 }
